f0eb9273508302769fb261aab79bd1760d45482f
Bhargav Sai Gajula <bhargavsai554@gmail.com>
2018-02-19 19:51:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var img=document.getElementById('image');
 var marginLeft=0;
 function moveRight(){
   
-    moveLeft=moveLeft+10;
+    marginLeft=marginLeft + 10;
     img.style.moveLeft= marginLeft + 'px';
 img.onclick=function(){
     var interval = setInterval(moveRight,100);
