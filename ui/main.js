64afac1b9ec4b78b91e6fbe8a4e4bf1e141576ca
Bhargav Sai Gajula <bhargavsai554@gmail.com>
2018-02-19 19:41:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,12 @@
 console.log('Loaded!');
-var img=document.getElementById('image');
+var img=document.getElementById('image'); 
+var marginLeft=0;
+function moveRight(){
+  
+    moveLeft=moveLeft+10;
+    img.style.moveLeft= marginLeft + 'px';
 img.onclick=function(){
-    var interval = setInterval(moveLeft,100);
+    var interval = setInterval(moveRight,100);
  
 };
+}
